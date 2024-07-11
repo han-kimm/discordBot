77372b137d66d7041545f9750c8e29d08950432b
Bungle <hn.kim@ecubelabs.com>
2024-07-11 16:29:18 +0900

    feat: 유저 프롬프트를 요약해서 multiqueryRetriever

--- src/components/retriever/multiqueryRetriever/confluence.ts
+++ src/components/retriever/multiqueryRetriever/confluence.ts
@@ -1,25 +1,42 @@
 import { Chroma } from "@langchain/community/vectorstores/chroma";
 import { chatOpenai, embedOpenai } from "../../../libs/openai";
 import { MultiQueryRetriever } from "langchain/retrievers/multi_query";
+import { PromptTemplate } from "@langchain/core/prompts";
 
 export async function confluenceMultiqueryRetriever(query: string) {
   const llm = chatOpenai("gpt-3.5-turbo");
 
-  const retriever = await Chroma.fromExistingCollection(
-    embedOpenai("text-embedding-3-small"),
+  const vertorStore = await Chroma.fromExistingCollection(
+    embedOpenai("text-embedding-3-large"),
     {
-      collectionName: "ecubelabs-multiquery",
+      collectionName: "ecubelabs-selfquery-2",
       url: "http://localhost:8000",
     }
-  ).then((chroma) =>
-    chroma.asRetriever({
-      k: 3,
-    })
   );
+
+  const retriever = vertorStore.asRetriever({
+    k: 1,
+  });
 
   const multiqueryRetriever = MultiQueryRetriever.fromLLM({
     retriever,
     llm,
+    prompt: new PromptTemplate({
+      inputVariables: ["question", "queryCount"],
+      template: `
+You are a good AI developer. Answer must always be based on given context. you can't lie.
+Generate 5 phrase which summarize the given context. the phrases will be used for searching relevant documents in company's database.
+
+Provide these alternative phrases separated by newlines between XML tags of 'questions'. For example:
+
+<questions>
+phrase 1
+phrase 2
+phrase 3
+</questions>
+
+context:{question}`,
+    }),
   });
 
   return multiqueryRetriever.invoke(query);
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import { confluenceMultiqueryRetriever } from "./components/retriever/multiqueryRetriever/confluence";
-import { confluenceSelfqueryRetriever } from "./components/retriever/selfqueryRetriever/confluence";
+import { chatOpenai } from "./libs/openai";
 
 async function main() {
-  const relatedDocs = await confluenceSelfqueryRetriever(
-    "I want documents about 'component'"
+  const relatedDocs = await confluenceMultiqueryRetriever(
+    `file name : apps/back-office-front/src/app/components/AddStatementOfAccountInvoiceDialog/AddStatementOfAccountInvoiceDialog.tsx`
   );
   console.log(relatedDocs);
 }
 
 main();
 
-async function query() {
-  const relatedDocs = await confluenceMultiqueryRetriever(``);
-  console.log(relatedDocs);
+async function chat() {
+  const llm = chatOpenai("gpt-3.5-turbo");
+  const response = await llm.invoke([
+    [
+      "system",
+      `You are a good AI developer. Answer must always be based on given file name. you can't lie.
+    Generate 3 keyword based on the given file name. the keywords will be used for searching relevant documents in company's database.`,
+    ],
+    [
+      "user",
+      `file name : apps/back-office-front/src/app/components/AddStatementOfAccountInvoiceDialog/AddStatementOfAccountInvoiceDialog.tsx`,
+    ],
+  ]);
+  console.log(response.content);
 }
